e6297e4b2464767667e5719ccf1cb3f094142e75
Dawid Świerczek <dawid.swierczek@gorrion.pl>
2016-05-22 10:29:35 +0200

    Poprawiono pobieranie slajdów

--- Core/app/modules/create-presentation/controllers/CreatePresentationCtrl.js
+++ Core/app/modules/create-presentation/controllers/CreatePresentationCtrl.js
@@ -1,12 +1,19 @@
 app.controller('CreatePresentationCtrl', function ($scope, $rootScope, $sce, CreatePresentationService) {
     
-    $rootScope.slides = CreatePresentationService.query({id: 1});
+    
+    CreatePresentationService.query({id: 1}).$promise.then(function (res) {
+        res.forEach(function (slide){ slide.Selected = false; });
+        $rootScope.slides = res;
+        var firstSlide = res[0];
+        firstSlide.Selected = true;
+        $rootScope.selectedSlide = firstSlide;
+    });
     
     $scope.$watch('slides', function (newVal, oldVal) { 
         CreatePresentationService.uploadPresentation({ id: 1, prezId: 1, data: $rootScope.slides});
     }, true);
     
-    $scope.editSlideMode = true;
+    $scope.viewSlideMode = true;
     $scope.markers = sharedFunctions.markers;
     
     $scope.addMark = function(tag) {
@@ -15,6 +22,8 @@ app.controller('CreatePresentationCtrl', function ($scope, $rootScope, $sce, Cre
     }
     
     $scope.renderSlideHtml = function(slide) {
+        if(!slide)
+            return;
         var html = sharedFunctions.toHtmlSlide(slide.Content);
         html = "<div style='background-color: " + slide.Background + ";'>" + html + "</div>"; 
         return $sce.trustAsHtml(html);
