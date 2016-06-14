47f043723332eac5bbd7cf0067be02dc66d6a714
Dawid Świerczek <dawid.swierczek@gorrion.pl>
2016-06-15 01:52:45 +0200

    Gadam z BoarBotem xD

--- Core/app/modules/main/controllers/MainCtrl.js
+++ Core/app/modules/main/controllers/MainCtrl.js
@@ -1,4 +1,39 @@
-app.controller('MainCtrl', ['$scope', '$rootScope', function ($scope, $rootScope) {
+app.controller('MainCtrl', ['$scope', '$rootScope', 'MainService', function ($scope, $rootScope, MainService) {
+    $scope.talkObj = {};
+    var latestMessage = null;
+
+    $scope.enableTalkMode = function () {
+        $scope.talkMode = true;
+        recognition.start();
+    };
+
+    $scope.disableTalkMode = function () {
+        $scope.talkMode = false;
+    };
+
+    recognition.onresult = function (event) {
+        for (var i = event.resultIndex; i < event.results.length; ++i) {
+            if (event.results[i].isFinal) {
+                recognition.stop();
+                var msg = event.results[i][0].transcript;
+                MainService.askBot({ ask: msg }).$promise.then(function(res){
+                    sharedFunctions.talk(res.message);
+                    recognition.start();
+                });
+            }
+        }
+    }
     
-    $scope.test = "TEST";
+    recognition.onerror = function (event) {
+        if(event.error != 'no-speech') {
+            sharedFunctions.talk('Przepraszam, nie zrozumiałem. Powtórz proszę.');
+            errorWasBefore = true;
+        }
+    }
+
+    recognition.onend = function () {
+        if($scope.talkMode)
+            recognition.start();
+    }
+    
 }]);
