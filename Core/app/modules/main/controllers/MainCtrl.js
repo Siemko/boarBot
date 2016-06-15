6dc6abc6a01aecbae8a77a7eee6ceb1da3f9c014
Dawid Świerczek <dawid.swierczek@gorrion.pl>
2016-06-15 02:37:44 +0200

    Animacja dzika

--- Core/app/modules/main/controllers/MainCtrl.js
+++ Core/app/modules/main/controllers/MainCtrl.js
@@ -1,6 +1,21 @@
 app.controller('MainCtrl', ['$scope', '$rootScope', 'MainService', function ($scope, $rootScope, MainService) {
     $scope.talkObj = {};
     var latestMessage = null;
+    var recognitionStarted = false;
+
+    $scope.animateBoar = function () {
+        var availableAnimations = ['bounce',
+            'pulse',
+            'rubberBand',
+            'shake',
+            'headShake',
+            'swing',
+            'tada',
+            'wobble',
+            'jello',];
+         var index = Math.floor((Math.random() * (availableAnimations.length -1)));
+         $scope.boarAnimation = availableAnimations[index];
+    }
 
     $scope.enableTalkMode = function () {
         $scope.talkMode = true;
@@ -9,6 +24,7 @@ app.controller('MainCtrl', ['$scope', '$rootScope', 'MainService', function ($sc
 
     $scope.disableTalkMode = function () {
         $scope.talkMode = false;
+        recognition.stop();
     };
 
     recognition.onresult = function (event) {
@@ -16,24 +32,31 @@ app.controller('MainCtrl', ['$scope', '$rootScope', 'MainService', function ($sc
             if (event.results[i].isFinal) {
                 recognition.stop();
                 var msg = event.results[i][0].transcript;
-                MainService.askBot({ ask: msg }).$promise.then(function(res){
+                MainService.askBot({ ask: msg }).$promise.then(function (res) {
                     sharedFunctions.talk(res.message);
-                    recognition.start();
+                    if(!recognitionStarted)
+                        recognition.start();
                 });
             }
         }
     }
-    
+
     recognition.onerror = function (event) {
-        if(event.error != 'no-speech') {
+        if (event.error != 'no-speech') {
             sharedFunctions.talk('Przepraszam, nie zrozumiałem. Powtórz proszę.');
             errorWasBefore = true;
         }
     }
 
     recognition.onend = function () {
-        if($scope.talkMode)
+        recognitionStarted = false;
+        if ($scope.talkMode)
             recognition.start();
     }
     
+    recognition.onstart = function () {
+        recognitionStarted = true;
+    }
+
+
 }]);
